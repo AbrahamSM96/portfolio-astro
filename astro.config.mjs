b7b59c91db720c624208deb60e6ca03789b7d174
Abraham Serrano <abrahamserrano96@hotmail.com>
2026-05-13 12:38:44 -0600

    feat: add sitemap integration and update related configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://abrahamsm.com',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   prefetch: {
     // Preload pages when their links enter the viewport (great with View Transitions)
     prefetchAll: true,
